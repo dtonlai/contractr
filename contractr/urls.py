339d5233ca50ac2f5f9d506de41f1d480fe97fd1
David Ton-Lai <dtonlai@ualberta.ca>
2018-12-27 18:10:12 -0700

    Added Accounts app

--- contractr/urls.py
+++ contractr/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
-from contractr_app import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.ContractrIndexView.as_view(), name='index'),
+    url(r'accounts/',include('django.contrib.auth.urls')),
 ]
+
+"""url(r"^accounts/", include("accounts.urls", namespace="accounts")),"""
--- contractr_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContractrAppConfig(AppConfig):
-    name = 'contractr_app'
